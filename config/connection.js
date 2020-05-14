508b89a45335dbdb2c5bc2f4c0143711d1d895d0
Derek Glassick <derek.glassick@gmail.com>
2020-05-13 19:46:32 -0700

    formatted employer form and made employee form

--- config/connection.js
+++ config/connection.js
@@ -10,7 +10,7 @@ var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "sqlpassword",
+  password: "12345678",
   database: "code_huntersdb"
 });
 
