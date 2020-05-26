567ca5e6285fd4601c3b5d2d55a07395172f2e63
Derek Glassick <derek.glassick@gmail.com>
2020-05-25 19:21:17 -0700

    added mysql changes with jawsdb

--- config/connection.js
+++ config/connection.js
@@ -6,14 +6,17 @@
 var mysql = require("mysql");
 
 // Set up our connection information
-var connection = mysql.createConnection({
-  host: "localhost",
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+}else{
+connection = mysql.createConnection({
+  host: "tuy8t6uuvh43khkk.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   port: 3306,
-  user: "root",
-  password: "12345678",
-  database: "code_huntersdb"
+  user: "vegyf9v09rp0ecme",
+  password: "mz4n60w94qeh0vw6",
+  database: "uutz7gau2wch3rhj"
 });
-
+};
 // Connect to the database
 connection.connect(function(err) {
   if (err) {
