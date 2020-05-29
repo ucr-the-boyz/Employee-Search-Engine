0f57285450c256db8090420ee2e7e981c3996dd5
Takyu Chan <takyu.chan958@gmail.com>
2020-05-29 14:50:58 -0700

    heroku push

--- config/connection.js
+++ config/connection.js
@@ -33,16 +33,21 @@
 // *********************************************************************************
 // Require mysql
 var mysql = require("mysql");
+var connection;
 // Set up our connection information
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "12345678",
-  database: "code_huntersdb"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "code_huntersdb"
+  });
+}
 // Connect to the database
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
